12ddbf583af6b83fb9b0bc4cc7d9c4ffcca0765d
Kaladar <ckaladar@gmail.com>
2022-08-10 15:15:01 +0530

    Able to create draggable circles through good functions

--- app.js
+++ app.js
@@ -5,11 +5,41 @@ function createCircle() {
 }
 
 function addElementToWebPage(element) {
-  console.log(element);
   document.body.appendChild(element);
 }
 
-addElementToWebPage(createCircle());
+function makePositionAbsolute(element) {
+  element.style.position = "absolute";
+}
+
+function setDraggableTrue(element) {
+  element.setAttribute("draggable", "true");
+}
+
+function setElementCoordinatesFromEvent(element, event) {
+  element.style.left = event.pageX + "px";
+  element.style.top = event.pageY + "px";
+}
+
+function changeElementPositionOnDrag(element) {
+  element.ondrag = function (event) {
+    setElementCoordinatesFromEvent(this, event);
+  };
+
+  element.ondragend = function (event) {
+    setElementCoordinatesFromEvent(this, event);
+  };
+
+  element.ondrag = function (event) {
+    setElementCoordinatesFromEvent(this, event);
+  };
+}
+
+const circle = createCircle();
+makePositionAbsolute(circle);
+setDraggableTrue(circle);
+changeElementPositionOnDrag(circle);
+addElementToWebPage(circle);
 
 const [btn] = document.getElementsByClassName("btn");
 
