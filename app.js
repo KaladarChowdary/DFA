27586880a07a05d05247841be50759553b5008ed
Kaladar <ckaladar@gmail.com>
2022-08-10 15:07:15 +0530

    Able to create single circle through pure functions

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+function createCircle() {
+  const circle = document.createElement("div");
+  circle.classList.add("circle");
+  return circle;
+}
+
+function addElementToWebPage(element) {
+  console.log(element);
+  document.body.appendChild(element);
+}
+
+addElementToWebPage(createCircle());
+
 const [btn] = document.getElementsByClassName("btn");
 
 btn.addEventListener("click", function (evt) {
